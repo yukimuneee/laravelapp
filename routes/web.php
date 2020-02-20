bded10131959b7d878c325e9938a57bc76c90493
yukimuneee <norugbynolife9@icloud.com>
2020-02-21 08:57:37 +0900

    get

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
+Route::get('/', function () {
+    return view('welcome');
+});
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body { font-size: 16pt; color: #999; }
+h1 { font-size: 100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello', function ()  use ($html){
+    return $html;
 });
