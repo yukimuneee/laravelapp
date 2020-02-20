d406e8ef3fa06e7d4892728e44859816a4b94087
yukimuneee <norugbynolife9@icloud.com>
2020-02-20 20:31:35 +0900

    setting

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('hello', function () {
+    return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
 });
