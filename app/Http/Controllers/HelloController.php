40d81b81441b7187169d0c3a2b0b0145d3e6c21a
yukimuneee <norugbynolife9@icloud.com>
2020-02-21 09:34:18 +0900

    controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index() {
+
+        return <<<EOF
+
+<html>
+<head>
+<title>Hello/Index</title>
+<style>
+body { font-size: 16pt; color: #999; }
+h1 { font-size: 100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>これは、Helloコントローラーのindexアクションです。</p>
+</body>
+</html>
+EOF;
+
+    }
 }
