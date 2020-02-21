6281a66d5c1298f0dfe834de62a3f6cd7c24f90f
yukimuneee <norugbynolife9@icloud.com>
2020-02-21 13:21:58 +0900

    複数コントローラの使い方

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,29 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+global $head, $style, $body, $end;
+$head = '<html></html>';
+$style = <<<EOF
+<style>
+body {font-size:16pt; color:#999; }
+h1 { font-size:100pt; text-aline:right; color:#eee; margin:-40px 0px -50px 0px;}
+</style>
+EOF;
+$body = '</head><body>';
+$end = '</body></html>';
+function tag($tag, $txt){
+    return "<{$tag}>" . $txt . "</{$tag}>";
+}
+
 class HelloController extends Controller
 {
-    public function index($id='noname', $pass='unknown') {
 
-        return <<<EOF
-
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body { font-size: 16pt; color: #999; }
-h1 { font-size: 100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
-</style>
-</head>
-<body>
-    <h1>Index</h1>
-    <p>これは、Helloコントローラーのindexアクションです。</p>
-    <ul>
-        <li>ID: {$id}</li>
-        <li>PASS: {$pass}</li>
-</body>
-</html>
-EOF;
+    public function index(){
+        global $head, $style, $body, $end;
+        $html =  $head . tag('title', 'Hello/Index') . $style . $body . tag('h1', 'Index') . tag('p', 'this is Index page') . '<a href="/hello/other">go to Other page</a>' . $end;
+        return $html;
 
     }
+    public function other(){
+        global $head, $style, $body, $end;
+        $html =  $head . tag('title', 'Hello/Other') . $style . $body . tag('h1', 'Other') . tag('p', 'this is Other page') . $end;
+        return $html;
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{id?}/{pass?}', 'HelloController@index');
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@other');
