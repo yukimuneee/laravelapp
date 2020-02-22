e67d095d2d6defae70217d3c572798fc92fba337
yukimuneee <norugbynolife9@icloud.com>
2020-02-22 20:10:55 +0900

    postメソッ勉強

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,7 +10,19 @@ class HelloController extends Controller
 
     public function index()
     {
-        return view('hello.index');
+        $data = [
+            'msg'=>'お名前を入力してください。',
+        ];
+        return view('hello.index', $data);
+
+    }
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg'=>'こんにちは' . $msg . 'さん',
+        ];
+        return view('hello.index', $data);
     }
 
 }
