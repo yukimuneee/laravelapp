240cf332092b89d631b82db17c9ff378d36e1a57
yukimuneee <norugbynolife9@icloud.com>
2020-02-22 19:16:26 +0900

    コントローラーでテンプレートを使う

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,30 +8,34 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
 
-    public function index(Request $request, Response $response){
-
-  $html = <<<EOF
-  <html>
-  <head>
-  <title>Hello/Index</title>
-  <style>
-  body {font-size:16pt; color:#999; }
-  h1 {font-size:120pt; text-aline:right; color:#fafafa; margin:-50px 0px -120px 0px;}
-  </style>
-  </head>
-  <body>
-    <h1>Hello</h1>
-    <h3>Request</h3>
-    <pre>{$request}</pre>
-    <h3>Response</h3>
-    <pre>{$response}</pre>
-  </body>
-  </html>
-  EOF;
-          $response->setContent($html);
-          return $response;
-
+    public function index()
+    {
+        return view('hello.index');
     }
+
+
+//   $html = <<<EOF
+//   <html>
+//   <head>
+//   <title>Hello/Index</title>
+//   <style>
+//   body {font-size:16pt; color:#999; }
+//   h1 {font-size:120pt; text-aline:right; color:#fafafa; margin:-50px 0px -120px 0px;}
+//   </style>
+//   </head>
+//   <body>
+//     <h1>Hello</h1>
+//     <h3>Request</h3>
+//     <pre>{$request}</pre>
+//     <h3>Response</h3>
+//     <pre>{$response}</pre>
+//   </body>
+//   </html>
+//   EOF;
+//           $response->setContent($html);
+//           return $response;
+
+//     }
 }
 
 
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 // Route::get('hello', 'HelloController@index');
 // Route::get('hello/other', 'HelloController@other');
 // Route::get('hello', 'HelloController@index');
-Route::get('hello', function () {
-    return view('hello.index');
-});
+// Route::get('hello', function () {
+//     return view('hello.index');
+// });
+Route::get('hello', 'HelloController@index');
