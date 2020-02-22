af8f7bfffd4a5fdc69b2aa0c31fcfade7f30553f
yukimuneee <norugbynolife9@icloud.com>
2020-02-22 19:27:22 +0900

    setting

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,29 +13,6 @@ class HelloController extends Controller
         return view('hello.index');
     }
 
-
-//   $html = <<<EOF
-//   <html>
-//   <head>
-//   <title>Hello/Index</title>
-//   <style>
-//   body {font-size:16pt; color:#999; }
-//   h1 {font-size:120pt; text-aline:right; color:#fafafa; margin:-50px 0px -120px 0px;}
-//   </style>
-//   </head>
-//   <body>
-//     <h1>Hello</h1>
-//     <h3>Request</h3>
-//     <pre>{$request}</pre>
-//     <h3>Response</h3>
-//     <pre>{$response}</pre>
-//   </body>
-//   </html>
-//   EOF;
-//           $response->setContent($html);
-//           return $response;
-
-//     }
 }
 
 
