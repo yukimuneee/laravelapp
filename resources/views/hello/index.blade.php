cf441b3e16a6accdec86cf5e6add53613890e242
yukimuneee <norugbynolife9@icloud.com>
2020-02-23 11:47:56 +0900

    setting

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -12,7 +12,8 @@
     <form method='POST' action="/hello">
       @csrf
       <input type='text' name="msg">
-      <input type='submit'>
+      
+      <input type='submit' >
     </form>
 </body>
 </html>
