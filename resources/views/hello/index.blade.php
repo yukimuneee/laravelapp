16a1e563b8ad4c3defd871905edd63b1e529d2fc
yukimuneee <norugbynolife9@icloud.com>
2020-02-24 19:41:44 +0900

    setting

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,14 +10,15 @@
     <h1>Blade/Index</h1>
     <p>&#064;foreachディレクティブの例</p>
     <ol>
-    @foreach ($data as $item)
-    @if ($loop->first)
-    <p>※データ一覧</p><ul>
-    @endif
-    <li>No,{{$loop->iteration}}.{{$item}}</li>
-    @if ($loop->last)
-    </ul><p>ーーここまで</p>
-    @endif
-    @endforeach
+    @php
+    $counter = 0;
+    @endphp
+    @while ($counter < count($data))
+    <li>{{$data[$counter]}}</li>
+    @php
+    $counter++;
+    @endphp
+    @endwhile
+    </ol>
 </body>
 </html>
