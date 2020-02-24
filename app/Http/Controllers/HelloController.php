1cdc02aa4f1b9e0ab108d3bcdaedcfe48cfdc92c
yukimuneee <norugbynolife9@icloud.com>
2020-02-24 18:24:23 +0900

    if文の学習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,19 +10,19 @@ class HelloController extends Controller
 
     public function index()
     {
-        $data = [
-            'msg'=>'お名前を入力してください。',
-        ];
-        return view('hello.index', $data);
+        // $data = [
+        //     'msg'=>'お名前を入力してください。',
+        // ];
+        return view('hello.index', ['msg'=>'']);
 
     }
     public function post(Request $request)
     {
-        $msg = $request->msg;
-        $data = [
-            'msg'=>'こんにちは' . $msg . 'さん',
-        ];
-        return view('hello.index', $data);
+        // $msg = $request->msg;
+        // $data = [
+        //     'msg'=>'こんにちは' . $msg . 'さん',
+        // ];
+        return view('hello.index', ['msg'=>$request->msg]);
     }
 
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,7 +8,11 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>{{$msg}}</p>
+    @if ($msg != '')
+    <P>こんにちは、{{$msg}}さん。</p>
+    @else
+    <p>何か書いて下さい。</p>
+    @endif
     <form method='POST' action="/hello">
       @csrf
       <input type='text' name="msg">
