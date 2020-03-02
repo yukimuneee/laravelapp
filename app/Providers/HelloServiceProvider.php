8b82800f8244accd81e488daafe71dfbdeab834e
yukimuneee <norugbynolife9@icloud.com>
2020-03-02 14:49:47 +0900

    ビューコンポーサ

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -2,27 +2,17 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class HelloServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        View::composer(
+            'hello.index', function($view){
+                $view->with('view_message', 'composer message!');
+            }
+        );
     }
 }
