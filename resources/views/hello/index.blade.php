c0b8e3df4e7a7520f96dbc8384c82befb98eabf2
yukimuneee <norugbynolife9@icloud.com>
2020-03-02 12:53:54 +0900

    @includeの使い方

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,8 +10,14 @@
 @section('content')
   <p>ここが本文のコンテンツです</p>
   <p>必要なだけ記述できます</p>
+
+
+  @include('components.message',['msg_title'=>'OK','msg_content'=>'サブビューです。'])
+
+
 @endsection
 
 @section('footer')
 copyright 2020 tuyano.
 @endsection
+
