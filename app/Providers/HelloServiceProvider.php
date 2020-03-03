7ba417dd950140a755e8d059acf2fced9d90eb55
yukimuneee <norugbynolife9@icloud.com>
2020-03-03 14:55:42 +0900

    setting

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -10,9 +10,7 @@ class HelloServiceProvider extends ServiceProvider
     public function boot()
     {
         View::composer(
-            'hello.index', function($view){
-                $view->with('view_message', 'composer message!');
-            }
+            'hello.index', 'App\Http\Composers\HelloComposer'
         );
     }
 }
